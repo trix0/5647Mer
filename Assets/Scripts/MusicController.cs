c9f1d2f444042df93c6f17e6665c8e6a6fe2714b
Trixo-Home <matus.satara@gmail.com>
2021-03-20 22:45:25 +0100

    Added sound

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System;
+public class MusicController : MonoBehaviour
+{
+    public Text currentSpeedText;
+    public Sound[] sounds;
+
+
+    public const string audioName = "merzedes-waltz-rythm-64bpm-05.wav";
+
+    [Header("Audio Stuff")]
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public string soundPath;
+
+    private void Awake()
+    {
+
+        foreach(Sound s in sounds)
+        {
+            s.source=gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+        }
+        Play("1.1");
+        Play("1.9");
+    }
+
+    public void Play(string name)
+    {
+        Sound s=Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Sound {
+
+    public int Level;
+    public int LevelOrder;
+    public string name;
+    [HideInInspector]
+    public AudioSource source;
+
+    public AudioClip clip;
+}
