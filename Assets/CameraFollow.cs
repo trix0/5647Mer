2b12dce1af389bbeb3849a7c4f031bb45e07546b
Trixo-Home <matus.satara@gmail.com>
2021-03-20 14:44:39 +0100

    Added some stuff

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -19,7 +19,8 @@ public class CameraFollow : MonoBehaviour
     private void HandleTranslation()
     {
         var targetPosition = target.TransformPoint(offset);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+        transform.position = targetPosition;
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
     }
     private void HandleRotation()
     {
